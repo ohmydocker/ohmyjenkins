bc8ed3d508ba53c34336584c0443db99d5ea89eb
Josh Cox <josh@webhosting.coop>
2017-01-16 22:12:13 -0600

    binary

--- rvmnvm.sh
+++ rvmnvm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-RUBY_TARGET=ruby-2.3.3
+RUBY_TARGET=ruby-2.2.4
 NODE_TARGET=lts/boron
 nvm install $NODE_TARGET
 nvm alias default $NODE_TARGET
 \curl -sSL https://get.rvm.io | bash -s stable
 source /var/jenkins_home/.rvm/scripts/rvm
-rvm install $RUBY_TARGET
+rvm install --binary $RUBY_TARGET
 rvm use --default $RUBY_TARGET
