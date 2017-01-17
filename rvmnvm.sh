14e7c75076f4d786e8cd8ff0a485235bc8767a3e
Josh Cox <josh@webhosting.coop>
2017-01-16 23:47:34 -0600

    pkg

--- rvmnvm.sh
+++ rvmnvm.sh
@@ -2,6 +2,8 @@
 RUBY_TARGET=ruby-2.3.3
 NODE_TARGET=lts/boron
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+export NVM_DIR="/var/jenkins_home/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install $NODE_TARGET
 nvm alias default $NODE_TARGET
 \curl -sSL https://get.rvm.io | bash -s stable
@@ -9,3 +11,5 @@ source /var/jenkins_home/.rvm/scripts/rvm
 rvm requirements
 rvm install --binary $RUBY_TARGET
 rvm use --default $RUBY_TARGET
+sudo tar zcf /opt/rvm.tgz .rvm
+sudo tar zcf /opt/nvm.tgz .nvm
